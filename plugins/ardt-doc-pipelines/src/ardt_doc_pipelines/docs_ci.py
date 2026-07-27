dcca3dfdf4e16c7a7ec521f4a7be1806f5b067b0
Thibault Poignonec <t.poignonec@asterion-robotics.com>
2026-07-27 12:23:52 +0200

    add file headers

--- plugins/ardt-doc-pipelines/src/ardt_doc_pipelines/docs_ci.py
+++ plugins/ardt-doc-pipelines/src/ardt_doc_pipelines/docs_ci.py
@@ -1,3 +1,20 @@
+# Copyright 2026 Asterion Robotics
+# SPDX-License-Identifier: Apache-2.0
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Thibault Poignonec <t.poignonec@asterion-robotics.com>
+
 """The ``docs-ci`` pipeline: a versioned documentation site under ``public/``.
 
 One build per version, each running the same ``ardt doc build`` task a dev
